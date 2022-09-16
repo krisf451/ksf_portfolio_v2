5b052d2442d5a147573d538579c4872a38fb4060
kfulkerson-pcg <kfulkerson@phxcapitalgroup.com>
2022-09-16 15:36:35 -0700

    small change to mobile navbar on about page, removing border on hover

--- src/pages/About/AboutNav.jsx
+++ src/pages/About/AboutNav.jsx
@@ -9,7 +9,7 @@ const AboutNav = () => (
   <div className="text-sm h-auto sm:h-12 flex justify-center items-center uppercase tracking-[.2em] w-full animate-slideleft my-4 lg:my-0">
     <ul className="flex flex-col gap-4 sm:gap-0 mb-4 sm:mb-0 sm:flex-row h-full items-center">
       {menus.map((item, i) => (
-        <li key={i} className="px-4 cursor-pointer hover:border-b-2">
+        <li key={i} className="px-4 cursor-pointer sm:hover:border-b-2">
           <a href={item.route}>
             <p className="pl-[.3rem]">{item.label}</p>
           </a>
